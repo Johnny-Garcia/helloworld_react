d90c5055fa531c1b6e97c885ece08ade69e7b3a4
Jonathan Garcia <jo-pg@hotmail.com>
2021-12-28 15:29:46 -0300

    Incluindo componetes com classe e função

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import HelloClassComponent from './component/helloClassComponent';
 import HelloFunctionComponent from './component/helloFunctionComponent';
 
 function App() {
   return (
-    <HelloFunctionComponent name="Jonathan Garcia"/>
+    <>
+      <HelloFunctionComponent name='Jonathan Garcia'/>
+      <HelloClassComponent name='Garcia Jonathan'/>
+    </>
   );
 }
 
